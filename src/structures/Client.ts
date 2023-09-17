c43ed94b86d7702451828880bb67a3e5b56fc9f7
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-17 22:54:56 +0300

    loadSlashCommands fonksiyonu oluşturuldu.

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,5 +1,6 @@
 import { Ping } from '@discord-point-bot/commands';
-import { MessageCreate, Ready } from '@discord-point-bot/events';
+import { InteractionCreate, MessageCreate, Ready } from '@discord-point-bot/events';
+import { SlashPing } from '@discord-point-bot/slash-commands';
 
 import { config } from '@config';
 import { ActivityType, Collection, Client as Core, GatewayIntentBits } from 'discord.js';
@@ -8,6 +9,7 @@ import { connect } from 'mongoose';
 
 export class Client extends Core {
   commands = new Collection<string, DiscordType.ICommand>();
+  slashCommands = new Collection<string, DiscordType.ISlashCommand>();
 
   constructor() {
     super({
@@ -17,14 +19,24 @@ export class Client extends Core {
   }
 
   private async loadCommands() {
-    const commands = [Ping];
+    const commands: DiscordType.ICommand[] = [Ping];
     await Promise.all(
       map(commands, async (command) => this.commands.set(command.usages[0], command)),
     );
   }
 
+  private async loadSlashCommands() {
+    const slashCommands: DiscordType.ISlashCommand[] = [SlashPing];
+    await Promise.all(
+      map(slashCommands, async (slashCommand) =>
+        this.slashCommands.set(slashCommand.data.name, slashCommand),
+      ),
+    );
+  }
+
   private async loadEvents() {
-    const events = [Ready, MessageCreate];
+    const events: DiscordType.IEvent[] = [Ready, MessageCreate, InteractionCreate];
+
     await Promise.all(
       map(events, async (event) =>
         this.on(event.name, (...args: unknown[]) => event.execute(this, [...args])),
@@ -33,7 +45,12 @@ export class Client extends Core {
   }
 
   async connect() {
-    await Promise.all([connect(config.DBACCESS), this.loadCommands(), this.loadEvents()]);
+    await Promise.all([
+      connect(config.DBACCESS),
+      this.loadCommands(),
+      this.loadEvents(),
+      this.loadSlashCommands(),
+    ]);
 
     await this.login(config.BOT_TOKEN);
   }
