dcfb2abbb586fac3c957845c4136633a144e936d
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-17 22:51:29 +0300

    feat: SlashCommandArgs ve ISlashCommand interfaces oluşturuldu

--- src/@types/discordType/index.d.ts
+++ src/@types/discordType/index.d.ts
@@ -1,4 +1,13 @@
-import { ClientEvents, Message } from 'discord.js';
+import {
+  CacheType,
+  ChatInputCommandInteraction,
+  ClientEvents,
+  Interaction,
+  Message,
+  MessageContextMenuCommandInteraction,
+  SlashCommandBuilder,
+  UserContextMenuCommandInteraction,
+} from 'discord.js';
 import { Client } from 'src/structures/Client';
 
 declare global {
@@ -14,8 +23,21 @@ declare global {
       args: string[];
     }
 
+    export interface SlashCommandArgs {
+      client: Client;
+      interaction:
+        | ChatInputCommandInteraction<CacheType>
+        | MessageContextMenuCommandInteraction<CacheType>
+        | UserContextMenuCommandInteraction<CacheType>;
+    }
+
     export type EventKeys = keyof ClientEvents;
     export type ArgsOf<K extends EventKeys> = ClientEvents[K];
+
+    export interface ISlashCommand {
+      data: SlashCommandBuilder;
+      execute: (slashCommandArgs: SlashCommandArgs) => Promise<void> | void;
+    }
 
     export interface IEvent {
       name: EventKeys;
