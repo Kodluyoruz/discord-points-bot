ac34deb455e8df44d10038858676586c2991a223
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-17 22:56:34 +0300

    feat: slash komutları için ready eventi düzenlendi

--- src/events/ready/Ready.ts
+++ src/events/ready/Ready.ts
@@ -1,6 +1,19 @@
+import { Events, SlashCommandBuilder } from 'discord.js';
+import { flatMap } from 'lodash';
+
 export const Ready: DiscordType.IEvent = {
-  name: 'ready',
-  execute: (client) => {
-    console.log(`${client.user.tag} is online!`);
+  name: Events.ClientReady,
+  execute: async ({ slashCommands, guilds, user, application }) => {
+    for (const guild of guilds.cache.map((g) => g)) {
+      try {
+        const commands: SlashCommandBuilder[] = flatMap(Array.from(slashCommands.values()), 'data');
+
+        await guild.commands.set(commands);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    console.log(`${user.tag} is online!`);
   },
 };
