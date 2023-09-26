bc525ce25e925ad26b004bc1daa2bb42f42e01bb
code-a-man <43219246+code-a-man@users.noreply.github.com>
2023-09-26 20:23:11 +0300

    feat: kullanıcının kendi puan ve sıralaması embedi eklendi

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { pointInfoEmbed } from './pointInfoEmbed';
+export { userPointsEmbed } from './userPointsEmbed';
---
+++ src/components/userPointsEmbed.ts
@@ -0,0 +1,28 @@
+import translation from '@translation';
+import { EmbedBuilder } from 'discord.js';
+import { Client } from 'src/structures/Client';
+
+type UserPointsEmbedProps = {
+  client: Client;
+  userData: UserPointsData;
+  period: string;
+};
+
+type UserPointsData = {
+  points: number;
+  place: number;
+  displayName: string;
+};
+
+export const userPointsEmbed = ({ client, userData, period }: UserPointsEmbedProps) => {
+  const embed = new EmbedBuilder().setColor(0x5865f2).setDescription(
+    translation('userPoints.description', {
+      name: userData.displayName,
+      points: userData.points,
+      place: userData.place,
+      period: period,
+    }),
+  );
+
+  return { embed };
+};
