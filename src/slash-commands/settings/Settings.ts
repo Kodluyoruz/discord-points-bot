5913a07fa60843641f5de5ec0ac0ed6a873c51b2
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-30 19:15:18 +0300

    feat: Settings ve Setup komutları oluşturuldu

---
+++ src/slash-commands/settings/Settings.ts
@@ -0,0 +1,15 @@
+import { settingsAdminEmbed } from '@discord-point-bot/components';
+
+import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+
+export const Settings: DiscordType.ISlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName('settings')
+    .setDescription('Setting Menüsünü Açar.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  execute: async ({ client, interaction, lang }) => {
+    const { embed, row } = await settingsAdminEmbed({ client, guild: interaction.guild, lang });
+
+    interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
+  },
+};
