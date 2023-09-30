0a937af6c466c7a00afde9768e9398da7cf5e585
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-30 19:22:18 +0300

    feat: guildCreate eventi oluşturuldu

---
+++ src/events/guildCreate/GuildCreate.ts
@@ -0,0 +1,19 @@
+import { setupEmbed } from '@discord-point-bot/components';
+
+import { ChannelType, Events, GuildBasedChannel, TextChannel } from 'discord.js';
+
+export const GuildCreate: DiscordType.IEvent = {
+  name: Events.GuildCreate,
+  execute: async (_, [guild]: DiscordType.ArgsOf<'guildCreate'>) => {
+    const filter = ({ type }: GuildBasedChannel) => type === ChannelType.GuildText;
+
+    const channel: TextChannel =
+      guild.systemChannel || (guild.channels.cache.find(filter) as TextChannel);
+
+    if (channel) {
+      const { embed, row } = await setupEmbed({ guild, lang: guild.preferredLocale });
+
+      channel.send({ embeds: [embed], components: [row] });
+    }
+  },
+};
