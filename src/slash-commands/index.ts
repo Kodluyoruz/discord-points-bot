ae12032dff1e59d8ba821f472bc4d9e7d7647f0a
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-30 19:15:18 +0300

    feat: Settings ve Setup komutları oluşturuldu

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -1,2 +1,5 @@
 export { PointInfo } from './pointInfo/PointInfo';
 export { UserPoints } from './userPoints/UserPoints';
+export { SlashPing } from './SlashPing/SlashPing';
+export { Setup } from './setup/Setup';
+export { Settings } from './settings/Settings';
---
+++ src/slash-commands/setup/Setup.ts
@@ -0,0 +1,15 @@
+import { setupEmbed } from '@discord-point-bot/components';
+
+import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+
+export const Setup: DiscordType.ISlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName('setup')
+    .setDescription('Kuruluma başlamak için mesaj yollar.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  execute: async ({ interaction, lang }) => {
+    const { embed, row } = await setupEmbed({ guild: interaction.guild, lang });
+
+    interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
+  },
+};
