8a7c8f1c82fcf0accc08e33456bb5de6ebf78971
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-30 19:22:18 +0300

    feat: guildCreate eventi oluşturuldu

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,4 @@
 export { Ready } from './ready/Ready';
 export { MessageCreate } from './messageCreate/MessageCreate';
 export { InteractionCreate } from './interactionCreate/InteractionCreate';
+export { GuildCreate } from './guildCreate/GuildCreate';
