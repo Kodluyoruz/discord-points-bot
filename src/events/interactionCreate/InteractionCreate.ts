990d73243149354ef77a1046bd1e7a2ca7640a22
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-09-30 19:05:47 +0300

    feat: button ve selectMenu için yapı oluşturuldu

--- src/events/interactionCreate/InteractionCreate.ts
+++ src/events/interactionCreate/InteractionCreate.ts
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, InteractionType } from 'discord.js';
 
 export const InteractionCreate: DiscordType.IEvent = {
   name: Events.InteractionCreate,
@@ -7,6 +7,18 @@ export const InteractionCreate: DiscordType.IEvent = {
       const command = client.slashCommands.get(interaction.commandName);
 
       command.execute({ client, interaction, lang: interaction.locale });
+    } else if (interaction.isButton()) {
+      const button = client.buttons.get(interaction.customId);
+
+      if (button) {
+        button.execute({ client, interaction, lang: interaction.locale });
+      }
+    } else if (interaction.isAnySelectMenu()) {
+      const selectMenu = client.selectMenus.get(interaction.customId);
+
+      if (selectMenu) {
+        selectMenu.execute({ client, interaction, lang: interaction.locale });
+      }
     }
   },
 };
