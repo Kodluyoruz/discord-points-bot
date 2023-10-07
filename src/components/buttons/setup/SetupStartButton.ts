d175594c1c9877b8bd045dc475f3cdcba2f5aa04
Onur-Morkoc <onurmorkoc00@gmail.com>
2023-10-08 00:01:33 +0300

    feat: info ve point kanal seçme özelliği eklendi.

--- src/components/buttons/setup/SetupStartButton.ts
+++ src/components/buttons/setup/SetupStartButton.ts
@@ -5,16 +5,20 @@ import { setupRoutes } from './routes';
 
 export const SetupStartButton = async ({ client, interaction, lang }: DiscordType.ButtonArgs) => {
   const settings = await GuildSettingsModel.findOne({ guildId: interaction.guild.id })
-    .select('logChannelId adminChannelId period')
+    .select('logChannelId adminChannelId point infoChannelId')
     .lean();
 
-  const { logChannelId, adminChannelId, point } = settings || {};
+  const { logChannelId, adminChannelId, point, infoChannelId } = settings || {};
 
   const subCustumId: keyof (typeof ButtonCustomId)['setup'] = !logChannelId
     ? 'log_channel'
     : !adminChannelId
     ? 'admin_channel'
+    : !infoChannelId
+    ? 'info_channel'
     : !point?.channelId
+    ? 'point_channel'
+    : !point?.period
     ? 'point_period'
     : 'done';
 
--- src/components/selectMenu/index.ts
+++ src/components/selectMenu/index.ts
@@ -1,3 +1,5 @@
 export { SetupLogSelectMenu } from './setup/SetupLogSelectMenu';
 export { SetupAdminSelectMenu } from './setup/SetupAdminSelectMenu';
 export { SetupPeriodSelectMenu } from './setup/SetupPeriodSelectMenu';
+export { SetupPointSelectMenu } from './setup/SetupPointSelectMenu';
+export { SetupInfoSelectMenu } from './setup/SetupInfoSelectMenu';
