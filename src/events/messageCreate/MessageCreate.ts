ac4402b4bd56f77a0c745dee5e5b012cbb4dd4f4
code-a-man <43219246+code-a-man@users.noreply.github.com>
2023-10-08 15:01:53 +0300

    feat: mesaj dinleme ve puanlama sistemi eklendi

--- src/events/messageCreate/MessageCreate.ts
+++ src/events/messageCreate/MessageCreate.ts
@@ -1,9 +1,28 @@
-import { Events } from 'discord.js';
+import { PointUnitType } from '@discord-point-bot/models';
+import { Events, TextChannel } from 'discord.js';
+import { UserPointModel } from 'src/models/point';
 
 export const MessageCreate: DiscordType.IEvent = {
   name: Events.MessageCreate,
-  execute: (client, [message]: DiscordType.ArgsOf<'messageCreate'>) => {
+  execute: async (client, [message]: DiscordType.ArgsOf<'messageCreate'>) => {
     if (message.author.bot || !message.guild) return;
 
+    const [guildId, userId, channelId, categoryId] = [
+      message.guild.id,
+      message.author.id,
+      message.channel.id,
+      (message.channel as TextChannel)?.parentId,
+    ];
+
+    const type = message.reference ? PointUnitType.REPLY : PointUnitType.TEXT;
+
+    await UserPointModel.pointAdd({
+      guildId,
+      userId,
+      type,
+      value: 1,
+      channelId,
+      categoryId,
+    });
   },
 };
--- src/models/pointUnits/constants/PointUnitType.ts
+++ src/models/pointUnits/constants/PointUnitType.ts
@@ -2,4 +2,5 @@ export enum PointUnitType {
   VOICE = 'VOICE',
   TEXT = 'TEXT',
   INVITE = 'INVITE',
+  REPLY = 'REPLY',
 }
